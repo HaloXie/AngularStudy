0237d32be7bd28ce087317be61f992d5e3ec09ff
1093660446@qq.com <>
2019-05-28 23:58:46 +0800

    custom the error handle and defined the log function

--- angular-tour-of-heroes/src/app/Handles/handleError.service.ts
+++ angular-tour-of-heroes/src/app/Handles/handleError.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
+// interFace
+import { ILog } from '../InterFace/ILog';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,14 +15,39 @@ export class HandleErrorService {
    * Let the app continue.
    * @param operation - name of the operation that failed
    * @param result - optional value to return as the observable result
+   * @returns - An anonymous method
    */
-  public handleError<T>(log: any, operation = 'operation', result?: T) {
+  public handleError_bak<T>(
+    log: (sring) => void,
+    operation = 'operation',
+    result?: T
+  ) {
     return (error: any): Observable<T> => {
       // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
 
       // TODO: better job of transforming error for user consumption
       log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   * @returns - An anonymous method
+   */
+  public handleError<T>(ILog: ILog, operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      ILog.Log(`${operation} failed: ${error.message}`);
 
       // Let the app keep running by returning an empty result.
       return of(result as T);
--- angular-tour-of-heroes/src/app/services/hero.service.ts
+++ angular-tour-of-heroes/src/app/services/hero.service.ts
@@ -11,6 +11,16 @@ import { catchError, map, tap } from 'rxjs/operators';
 // Handle
 import { HandleErrorService } from '../Handles/handleError.service';
 
+// InterFace
+import { ILog } from '../InterFace/ILog';
+
+class CustomLog implements ILog {
+  constructor(private messageService: MessageService) {}
+  Log(str: string): void {
+    this.messageService.add(str);
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,45 +29,48 @@ export class HeroService {
     private http: HttpClient,
     private handleErrorService: HandleErrorService,
     private messageService: MessageService
-  ) {}
+  ) {
+    this.customLog = new CustomLog(this.messageService);
+  }
+
+  // Http
+  // URL to web api
+  private heroesUrl = 'api/heroes';
+
+  private customLog: CustomLog;
 
   getHeroes(): HeroModel[] {
     return HEROES;
   }
 
-  //Sync
+  // Sync
   getObservableHeroes(): Observable<HeroModel[]> {
     this.messageService.add('HeroService: fetched getObservableHeroes');
     return of(HEROES);
   }
 
-  //Sync
+  // Sync
   getObservableHero(id: number): Observable<HeroModel> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(x => x.id === id));
   }
 
-  // Http
-
-  private heroesUrl = 'api/heroes'; // URL to web api
-
-  /** Log a HeroService message with the MessageService */
-  private log(message: string) {
-    this.messageService.add(`HeroService: ${message}`);
-  }
-
   /** GET heroes from the server */
-  // HttpClient.get returns the body of the response as an untyped JSON object by default. Applying the optional type specifier, <HeroModel[]> , gives you a typed result object.
   getHttpHeroes(): Observable<HeroModel[]> {
-    return this.http.get<HeroModel[]>(this.heroesUrl).pipe(
-      tap(_ => this.log('fetched heroes')),
-      catchError(
-        this.handleErrorService.handleError<HeroModel[]>(
-          this.log,
-          'getHeroes',
-          []
+    return (
+      this.http
+        .get<HeroModel[]>(this.heroesUrl)
+        // Now extend the observable result with the .pipe() method and give it a catchError() operator.
+        .pipe(
+          tap(() => this.customLog.Log('fetched heroes')),
+          catchError(
+            this.handleErrorService.handleError<HeroModel[]>(
+              this.customLog,
+              'getHeroes',
+              []
+            )
+          )
         )
-      )
     );
   }
 }
